7bfe1ac76325f3afdcb94549c5d9433cc66d754b
Grumblesaur <jmurphy4@mail.umw.edu>
2014-12-03 21:03:41 -0500

    Final 2. Comments added to bucket sort algorithm.

--- algorithm.cpp
+++ algorithm.cpp
@@ -4,22 +4,33 @@
 using namespace std;
 
 void bucketSort(int array[], int arraySize, int numBuckets){
+	//numBuckets is passed in as the MOD value used on rand() in main
+	
+	//create a bucket for each possible value in the array
 	int buckets[numBuckets];
 	
+	//empty all the buckets
 	for (int i = 0; i < numBuckets; i++){
 		buckets[i] = 0;
 	}
 	
+	//for each int in the array
 	for (int j = 0; j < arraySize; j++){
+		//increment the bucket that holds the value found at index j of the array
+		//e.g. if array[j] is 47, then buckets[47] is increased by one
 		buckets[array[j]]++;
 	}
 	
+	//create loop variables to avoid stepping outside the bounds of buckets[] and array[]
 	int n = 0;
 	int m = 0;
 	while (n < numBuckets && m < arraySize){
+		//for as many of each number is found in a bucket
 		for (int k = buckets[n]; k > 0; k--){
+			//put that number into its sorted place in the array
 			array[m++] = n;
 		}
+		//move onto the next bucket
 		n++;
 	}	
 	return;
